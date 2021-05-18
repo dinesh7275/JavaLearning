49b3cbf7d5ebb0b76db3a4f6b1d0f00facf0c4d7
dinesh7275 <chandra.dinesh74@gmail.com>
2021-05-18 22:51:21 +0530

    new  poly

---
+++ src/recursion/Factorial.java
@@ -0,0 +1,22 @@
+package recursion;
+
+import static recursion.Factorial.factorial;
+
+public class Factorial {
+    public static double factorial(double n) {
+        if (n == 1)
+        return n;
+        else{
+            return (n* factorial(n-1));
+    }
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        double fact= factorial(5);
+        System.out.println(fact);
+
+    }
+}
